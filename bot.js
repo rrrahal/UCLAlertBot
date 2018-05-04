235cb8c6cd76877039eb60d7f761426d03f9c821
Rafael Rahal <rrrahal@gmail.com>
2018-05-04 16:52:56 -0300

    create simple app and config.json

--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('Ready!');
 });
 
-client.login('my-token');
+client.on('message',(message) => {
+    if (message.content == "!ping")
+        console.log("Pong!");
+    else {
+        console.log('Não entendi, não');
+    }
+});
+
+client.login(config.token);
