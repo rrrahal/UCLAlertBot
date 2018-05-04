45cef89388ac59956803858f269d84826dab3726
Rafael Rahal <rrrahal@gmail.com>
2018-05-04 17:30:27 -0300

    add 'cloreto' feature

--- bot.js
+++ bot.js
@@ -1,17 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 client.on('ready', () => {
     console.log('Ready!');
+    let canal2;
+    client.channels.array().forEach((channel) => {
+        if (channel.name === 'segundo-canal') {
+            canal2 = channel;
+        }
+    });
 });
 
 client.on('message',(message) => {
-    if (message.content == "!ping")
-        console.log("Pong!");
-    else {
-        console.log('Não entendi, não');
+    if (message.content.includes('cloreto') && !message.author.bot) {
+        message.channel.send("É, você realmente deveria atualizar o cloreto.");
     }
 });
 
-client.login(config.token);
+client.login(token);
